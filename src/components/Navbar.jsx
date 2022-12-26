1d4cf5d57c10645a22a73c58b9e016558d3e3a34
M-an-o-j <ronaldomano7070@gmail.com>
2022-12-26 15:59:29 +0530

    chnaging z value of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             className='fixed top-4 left-4 z-20 md:hidden w-8 bg-secondary p-[5px] rounded-full' 
             onClick={() => setMenu(!menu)}
             />
-          <ul className={`p-20 md:p-0 h-screen md:h-auto md:flex fixed md:left-5  md:top-0 flex-col md:bg-primary bg-secondary  md:flex-row ${menu ? "left-0" : "-left-96"} duration-500 rounded-tr-2xlm rounded-br-2xl ease-in-out `}>
+          <ul className={`p-20 md:p-0 h-screen md:h-auto md:flex fixed md:left-5 z-50 md:top-0 flex-col md:bg-primary bg-secondary  md:flex-row ${menu ? "left-0" : "-left-96"} duration-500 rounded-tr-2xlm rounded-br-2xl ease-in-out `}>
             {Navlist.map((nav,index) => (
               <li className='md:mr-10 md:mb-0 mb-10 md:mt-0 mt-10  text-center text-xl font-NotoSans font-semibold text-text-color' key={index}><a href={nav.id}>{nav.navname}</a></li>
             ))}
